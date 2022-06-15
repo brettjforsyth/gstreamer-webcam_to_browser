e41dd19e91138256bb802ba3d5ea6f79e9f4cfd2
Brett Forsyth <info@brettforsyth.com>
2022-06-15 15:24:50 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -36,17 +36,16 @@ class CamWSHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         #print (message)
-        match message:
-            case 'large':
-                print('large capture')
-                os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 640 --height 480 -n --immediate')
-            case 'small':
-                print('small small capture')
-                os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 320 --height 240 -n --immediate')
-            case 'capture_raw':
-                list = os.listdir('/home/brett/Documents/gstreamer-webcam_to_browser/hires_images') # dir is your directory path
-                number_files = len(list)+1        
-                os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/hires_images/image_'+number_files+'.dng -r -n --immediate')
+        if message == 'large':
+            print('large capture')
+            os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 640 --height 480 -n --immediate')
+        elif message == 'small':
+            print('small small capture')
+            os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 320 --height 240 -n --immediate')
+        elif message == 'capture_raw':
+            list = os.listdir('/home/brett/Documents/gstreamer-webcam_to_browser/hires_images') # dir is your directory path
+            number_files = len(list)+1        
+            os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/hires_images/image_'+number_files+'.dng -r -n --immediate')
         send_all(str('image captured'))
 
     def on_close(self):
