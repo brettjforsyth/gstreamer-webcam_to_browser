6a3e9e5054fc330d031d70bb7490876fdf33ced5
Brett Forsyth <info@brettforsyth.com>
2022-06-16 09:21:27 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -130,6 +130,7 @@ if __name__ == "__main__":
         (r'/ws', CamWSHandler),
         (r'/', HTTPServer),
         (r'/files', HTTPServer2),
+        (r'/lores_images/(.*)', tornado.web.StaticFileHandler, {'path':'/home/brett/Documents/gstreamer-webcam_to_browser/lores_images'}),
         (r"/(preview.jpg)", tornado.web.StaticFileHandler, {'path':'./'}),
         (r"/(bulma.min.css)", tornado.web.StaticFileHandler, {'path':'./'})        
     ])
