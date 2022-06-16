6522753677029d7cfa894f749070a677a58b11b3
Brett Forsyth <info@brettforsyth.com>
2022-06-15 20:46:11 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -41,7 +41,7 @@ class CamWSHandler(tornado.websocket.WebSocketHandler):
             os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 640 --height 480 -n --immediate')
         elif message == 'small':
             print('small small capture')
-            os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 2328 --height 1748 -n --flush -q 20 --immediate')
+            os.system('sudo libcamera-still -o /home/brett/Documents/gstreamer-webcam_to_browser/preview.jpg --width 2328 --height 1748 -n --flush -q 20 --immediate --autofocus')
         elif message == 'capture_raw':
             list = os.listdir('/home/brett/Documents/gstreamer-webcam_to_browser/hires_images') # dir is your directory path
             number_files = len(list)+1        
